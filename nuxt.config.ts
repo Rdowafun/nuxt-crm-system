ec2d1f27a820e562e20a4b715fbc05d7b2cdf010
Rdowafun <jabrooster@gmail.com>
2024-04-28 11:15:07 +0500

    edit nuxt config

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,5 +1,9 @@
 export default defineNuxtConfig({
   devtools: { enabled: false },
+  app: {
+    baseURL: '/nuxt-crm-system/', // baseURL: '/<repository>/'
+    buildAssetsDir: 'assets' // don't use "_" at the begining of the folder name to avoids nojkill conflict
+  },
   modules: [
     '@nuxt/ui',
     '@nuxt/image',
