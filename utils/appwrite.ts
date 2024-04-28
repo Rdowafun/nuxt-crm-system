e489801c1c76e97734befc205823d619d08b9c81
Aziz Riskulov <vinceere@gmail.com>
2024-04-28 22:24:25 +0500

    Update appwrite.ts
    Cors policy check fix try

--- utils/appwrite.ts
+++ utils/appwrite.ts
@@ -4,6 +4,9 @@ import { Account, Client, Databases, Storage } from 'appwrite'
 export const client = new Client()
 
 client.setEndpoint('https://cloud.appwrite.io/v1').setProject(APP_WRITE_ID)
+export default async ({ req, res, log, error }) => {
+    res.send('I am alive!', 200, { "Access-Control-Allow-Origin": "*" });
+};
 
 export const account = new Account(client)
 export { ID } from 'appwrite'
