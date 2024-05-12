366f2a447cd931d9aee4895ff54684fb79e34857
Rdowafun <jabrooster@gmail.com>
2024-05-12 21:57:24 +0500

    edit client url

--- utils/appwrite.ts
+++ utils/appwrite.ts
@@ -3,7 +3,7 @@ import { Account, Client, Databases, Storage } from 'appwrite'
 
 export const client = new Client()
 
-client.setEndpoint('https://rdowafun.github.io').setProject(APP_WRITE_ID)
+client.setEndpoint('https://cloud.appwrite.io/v1').setProject(APP_WRITE_ID)
 export const account = new Account(client)
 export { ID } from 'appwrite'
 export const DB = new Databases(client)
