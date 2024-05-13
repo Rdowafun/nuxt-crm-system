a5c4424dcb78a6d5d5a1c0ca3c4635f10551ad14
Rdowafun <jabrooster@gmail.com>
2024-05-13 20:18:16 +0500

    delete custom headers

--- pages/login.vue
+++ pages/login.vue
@@ -1,92 +1,73 @@
 <script setup lang="ts">
-import { v4 as uuid } from "uuid";
-import { useAuthStore, useIsLoadingStore } from "~/store/auth.store";
+import { v4 as uuid } from 'uuid'
 
-const store = useAuthStore();
 useSeoMeta({
-  title: "Login | CRM System",
-});
+	title: 'Login | CRM System',
+})
 
-const emailRef = ref("");
-const passwordRef = ref("");
-const nameRef = ref("");
+const emailRef = ref('')
+const passwordRef = ref('')
+const nameRef = ref('')
 
-const isLoadingStore = useIsLoadingStore();
-const authStore = useAuthStore();
-const router = useRouter();
-
-onMounted(async () => {
-  if (store.isAuth) {
-    await account.deleteSession("current");
-    store.clear();
-  }
-});
+const isLoadingStore = useIsLoadingStore()
+const authStore = useAuthStore()
+const router = useRouter()
 
 const login = async () => {
-  const headers = {"Access-Control-Allow-Origin": "*"}
-  isLoadingStore.set(true);
-  await account.createEmailPasswordSession(emailRef.value, passwordRef.value, headers);
-  const response = await account.get();
-  if (response) {
-    authStore.set({
-      email: response.email,
-      name: response.name,
-      status: response.status
-    });
-  }
+	isLoadingStore.set(true)
+	await account.createEmailPasswordSession(emailRef.value, passwordRef.value)
+	const response = await account.get()
+	if (response) {
+		authStore.set({
+			email: response.email,
+			name: response.name,
+			status: response.status,
+		})
+	}
 
-  emailRef.value = "";
-  passwordRef.value = "";
-  nameRef.value = "";
+	emailRef.value = ''
+	passwordRef.value = ''
+	nameRef.value = ''
 
-  await router.push("/");
-  isLoadingStore.set(false);
-};
+	await router.push('/')
+	isLoadingStore.set(false)
+}
+
 const register = async () => {
-  const headers = {"Access-Control-Allow-Origin": "*"}
-  await account.create(
-    uuid(),
-    emailRef.value,
-    passwordRef.value,
-    nameRef.value,
-    headers
-  );
-  await login();
-};
+	await account.create(uuid(), emailRef.value, passwordRef.value, nameRef.value)
+	await login()
+}
 </script>
+
 <template>
-  <div class="flex items-center justify-center min-h-screen">
-    <div class="rounded bg-sidebar w-1/4 p-5">
-      <h1 class="text-2xl font-bold text-center mb-5">Login</h1>
-      <form>
-        <UiInput
-          v-model="emailRef"
-          placeholder="Email"
-          type="email"
-          class="mb-3"
-        />
-        <UiInput
-          v-model="passwordRef"
-          placeholder="Password"
-          type="password"
-          class="mb-3"
-        />
-        <UiInput
-          v-model="nameRef"
-          placeholder="Name"
-          type="name"
-          class="mb-3"
-        />
-        <div class="flex items-center justify-center gap-5">
-          <UiButton type="button" @click="login">Login</UiButton>
-          <UiButton type="button" @click="register">Register</UiButton>
-        </div>
-      </form>
-    </div>
-  </div>
+	<div class="flex items-center justify-center min-h-screen w-full">
+		<div class="rounded bg-sidebar w-1/4 p-5">
+			<h1 class="text-2xl font-bold text-center mb-5">Login</h1>
+
+			<form>
+				<UiInput
+					placeholder="Email"
+					type="email"
+					class="mb-3"
+					v-model="emailRef"
+				/>
+				<UiInput
+					placeholder="Password"
+					type="password"
+					class="mb-3"
+					v-model="passwordRef"
+				/>
+				<UiInput
+					placeholder="Name"
+					type="name"
+					class="mb-3"
+					v-model="nameRef"
+				/>
+				<div class="flex items-center justify-center gap-5">
+					<UiButton type="button" @click="login">Login</UiButton>
+					<UiButton type="button" @click="register">Register</UiButton>
+				</div>
+			</form>
+		</div>
+	</div>
 </template>
-
-
-
-<style>
-</style>
